8fce4d7ca36cbaac7ca0a1dc1f9d053e15349c13
matike16 <lda240820@gmail.com>
2024-07-30 19:25:18 +0800

    切换live2d-widget和live2d_api资源库镜像源
    将live2d-widget和live2d_api的资源库镜像源从stevenjoezhang和fghrsh切换到matike16，并更新了相应的路径。这次更改不仅更新了资源的获取路径，还优化了代码中的注释，提高了代码的可读性和维护性。

--- autoload.js
+++ autoload.js
@@ -1,6 +1,7 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+//const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/matike16/live2d-widget@master/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -35,7 +36,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			// cdnPath: "https://fastly.jsdelivr.net/gh/matike16/live2d_api/",
+			cdnPath: "https://cdn.staticaly.com/gh/matike16/live2d_api@master/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
